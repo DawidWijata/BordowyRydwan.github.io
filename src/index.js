26f30f6d4961bc8917f58672785dfa765185f64d
Dawid Wijata <dawid.wijata@hotmail.com>
2021-03-15 13:00:09 +0100

    Delete fix of image height detection function

--- src/index.js
+++ src/index.js
@@ -2,15 +2,3 @@ import "./js/lang.js";
 import "./js/menu.js";
 import "./js/caroussel.js";
 import "./js/scroll.js";
-
-const curves = document.querySelectorAll('.bg-curve');
-
-const fn = () => {
-  const DIMENSIONS_RATIO = 8.421;
-  const controlUnitHeight = Math.floor(window.innerWidth / DIMENSIONS_RATIO);
-
-  curves.forEach(curve => curve.style.height = `${controlUnitHeight}px`);
-}
-
-//window.addEventListener('resize', fn);
-//window.addEventListener('DOMContentLoaded', fn);
