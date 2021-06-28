65a7a2f6e4293e147c1fc8a44ebcf4a78fecc27e
Dawid Wijata <dawid.wijata@hotmail.com>
2021-06-28 16:58:06 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ git init
 echo 'dawidwijata.pl' > CNAME
 git add -A
 
+git config user.email "dawid.wijata@hotmail.com"
+git config user.name "Dawid Wijata"
+
 git remote add origin https://github.com/BordowyRydwan/BordowyRydwan.github.io.git
 git commit -m 'deploy'
 git push -f origin master:gh-pages
